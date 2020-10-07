dcce931b7c46ce4ba30804dc0a1abc40cf249106
MarkBasov <34864073+MarkBasov@users.noreply.github.com>
2020-10-07 10:53:36 +0500

    Update index.js

--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,8 +1,28 @@
 import { createStore } from "vuex";
+import Axios from 'axios';
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    city: null,
+  },
+  mutations: {
+    setCity: (state, loadedData) => {
+      state.city = loadedData;
+    },
+  },
+  getters: {
+    cities: (state) => {
+      return state.city;
+    }
+  },
+  actions: {
+    getCity: async (context, datas) => {
+      let {data} = await Axios({
+        method: 'get',
+        url: https://www.metaweather.com/api/location/${datas.item.uid}/${datas.date.toISOString().split('T')[0].replace(/-/g, '/')}
+      });
+      context.commit('setCity', data.data);
+    },
+  },
   modules: {}
 });
